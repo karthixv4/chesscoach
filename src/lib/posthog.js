2e52d49cbdae7b939ae64c9eb6ea81e5eadc3399
vigneshkarthick <vigneshkarthicknk@gmail.com>
2026-05-11 21:47:52 +0530

    disabled session recordings

--- src/lib/posthog.js
+++ src/lib/posthog.js
@@ -4,12 +4,7 @@ posthog.init('phc_zF3u6Ddnu5xzV5RPLk8BBZPUSrdmAgJR6DZu8vF9T3Ef', {
     api_host: 'https://us.i.posthog.com',
     autocapture: false,
     capture_pageview: false, // we handle pageviews manually via PostHogPageTracker
-    session_recording: {
-        maskAllInputs: true,       // hides typed text in replays (privacy)
-        maskInputOptions: {
-            password: true,        // double-ensure passwords are masked
-        },
-    },
+    session_recording: false,
 })
 
 export default posthog
